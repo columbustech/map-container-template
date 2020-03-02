26c6b703b2c78a72de4688107f60d1775a2ec037
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2020-03-02 19:31:58 +0000

    Changed input to cdrive path instead of a download url in views

--- src/foo/views.py
+++ src/foo/views.py
@@ -6,12 +6,23 @@ from rest_framework.views import APIView
 from rest_framework import status
 
 from .process import *
+import requests
 
 class ProcessView(APIView):
     parser_class = (JSONParser,)
 
     @csrf_exempt
     def post(self, request): 
-        downloadUrl = request.data['downloadUrl']
-        df = process(downloadUrl)
+        download_url = request.data['downloadUrl']
+        access_token = request.data['accessToken']
+
+        s3_url = None
+        for i in range(0, 10):
+            try:
+                resp = requests.get(url=download_url, headers={'Authorization':'Bearer ' + access_token})
+                s3_url = resp.json()['download_url']
+            except:
+                continue
+            break
+        df = process(s3_url)
         return Response({"output": df.to_dict('records')}, status=status.HTTP_200_OK)
